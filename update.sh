c8ef3cfd2dded44cf5b7f8f7fa2596508d77e018
James Shank <145942792+MaLsR6053@users.noreply.github.com>
2024-06-14 07:55:50 -0500

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,37 +1,76 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
+echo "
+.----------------------------------------------------------------------------.
+|  ____  _                 _    _       _   _           _       _            |
+| / ___|| |__   __ _ _ __ | | _( )___  | | | |_ __   __| | __ _| |_ ___ _ __ |
+| \___ \| '_ \ / _\` | '_ \| |/ /// __| | | | | '_ \ / _\` |/ _\` | __/ _ \ '__||
+|  ___) | | | | (_| | | | |   <  \__ \ | |_| | |_) | (_| | (_| | ||  __/ |   |
+| |____/|_| |_|\__,_|_| |_|_|\_\ |___/  \___/| .__/ \__,_|\__,_|\__\___|_|   |
+|                                            |_|                             |
+'----------------------------------------------------------------------------'
+"
 echo " "
 echo "Preparing to get the standard update."
+
 echo " "
 sudo apt-get update -y
 echo " "
-echo "The standard update has finished... "
+echo "
+***************************************
+    *****  Update Completed!  *****
+***************************************
+"
 echo " "
 sleep 2
 echo "Preparing to get the standard upgrade."
 echo " "
 sudo apt-get upgrade -y
 echo " "
-echo "The standard upgrade has finished... "
+echo "
+***************************************
+   *****  Upgrade Completed!  *****
+***************************************
+"
 echo " "
 sleep 2
 echo "Preparing to get the full upgrade."
 echo " "
 sudo apt-get full-upgrade -y
 echo " "
-echo "The full upgrade has finished..."
+echo "
+***************************************
+ *****  Full-Upgrade Completed!  *****
+***************************************
+"
 echo " "
 sleep 2
 echo "Preparing to complete autocleaning and auto removal of packages."
 echo " "
 sudo apt autoclean -y
 echo " "
-echo "Autoclean has finished..."
+echo "
+***************************************
+   *****  Autoclean Complete!  *****
+***************************************
+"
 echo " "
 sudo apt autoremove -y
 echo " "
-echo "Autoremove has finished..."
+echo "
+***************************************
+   ****  Autoremove Complete!  *****
+***************************************
+"
 echo " "
 echo " "
 sleep 3
-echo "The update has finished."
+# ASCII art completion message
+echo "
+***************************************
+ ****  Update Script Completed!  *****
+***************************************
+"
+
+# End of script
+
